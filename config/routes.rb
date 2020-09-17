21611a5932eae214e753d2e9dca68cad7ac45e83
kamihiro0626 <hiroya.kamishioiri@gmail.com>
2020-09-17 11:44:04 +0900

    細部修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "tops#index"
+  root "homes#top"
   resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_055504) do
+ActiveRecord::Schema.define(version: 2020_09_16_112945) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_055504) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
